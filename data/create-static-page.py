cf27fcffd88a03b272394382830462d7ada87e38
Tim Hagen <timhagenmail@googlemail.com>
2024-09-25 10:49:48 +0200

    download the frontend

--- data/create-static-page.py
+++ data/create-static-page.py
@@ -3,21 +3,11 @@ import click
 from pathlib import Path
 from tira.rest_api_client import Client
 from tqdm import tqdm
+import requests
+import zipfile
+import io
 import json
 
-
-def html():
-        return f"""
-<!DOCTYPE html>
-<html>
-<head>
-<title>data.tira.io</title>
-</head>
-<body>
-<h1>data.tira.io</h1>
-Data api for <a href="https://tira.io">TIRA.io</a> (currently in alpha).
-</body>
-</html>"""
 
 def persist(output_dir, endpoint):
     tira = Client()
@@ -33,17 +23,18 @@ def persist(output_dir, endpoint):
 
     return response
 
+def extract_zip_from_url(url: str, to: Path) -> None:
+    with requests.get(url, stream=True) as req:
+        with zipfile.ZipFile(io.BytesIO(req.content)) as zip_ref:
+            zip_ref.extractall(to)
+
+
 @click.command('create-static-page')
 @click.argument('output_dir', type=Path)
 def main(output_dir: Path):
-    output_dir.mkdir(exist_ok=True, parents=True)
-    with open(output_dir / 'index.html', 'w') as f:
-        f.write(html())
-
-    (output_dir / 'tira-data').mkdir(exist_ok=True, parents=True)
-    with open(output_dir / 'tira-data' / 'index.html', 'w') as f:
-        f.write(html())
     print(output_dir)
+
+    extract_zip_from_url('https://github.com/tira-io/tira/releases/latest/download/frontend-build.zip', output_dir)
     tasks = persist(output_dir, "/api/task-list")['context']['task_list']
     for task in tqdm(tasks, 'Persist tasks'):
         datasets = persist(output_dir, f'/api/datasets_by_task/{task["task_id"]}')
