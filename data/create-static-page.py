eaa0321907874db5847d6f13bc9fc117ff677ce9
Maik Fröbe <mam10eks@users.noreply.github.com>
2025-03-22 12:36:03 +0100

    Update create-static-page.py

--- data/create-static-page.py
+++ data/create-static-page.py
@@ -40,7 +40,7 @@ def extract_zip_from_url(url: str, to: Path) -> None:
 def main(output_dir: Path):
     print(output_dir)
 
-    extract_zip_from_url('https://github.com/tira-io/tira/releases/download/0.0.137-ui7/frontend-build.zip', output_dir)
+    extract_zip_from_url('https://github.com/tira-io/tira/releases/download/0.0.137-ui9/frontend-build.zip', output_dir)
     shutil.copyfile(output_dir / "index.html", output_dir / "404.html")
 
     persist(output_dir, "/api/role")
